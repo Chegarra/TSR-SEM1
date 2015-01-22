a0d4e9fd9189be6f2f0265ad580d93937d7e4c43
Chegarra <chegarra@outlook.com>
2015-01-22 16:14:19 +0100

    Subiendo el Agente

---
+++ agent.js
@@ -0,0 +1,19 @@
+var net = require('net');
+
+var agent = net.connect({port:8000}, function() {
+	console.log('Agente conectado');
+		
+	agent.write('Mundo\r\n');
+});
+	
+		
+agent.on('data', function(data) {
+		
+	console.log(data.toString());
+	
+	agent.end();
+});
+			
+agent.on('end', function() {
+	console.log('Cliente desconectado');
+});				
